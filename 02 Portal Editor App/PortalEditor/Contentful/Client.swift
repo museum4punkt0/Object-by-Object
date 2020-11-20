889ecacd1deb71f93af2cc5bca0fb3b391207f04
Ekkehard Petzold <mail@programmator.de>
2020-11-20 12:14:33 +0100

    Migrated CMS Access Credentials to property list file

--- 02 Portal Editor App/PortalEditor/Contentful/Client.swift
+++ 02 Portal Editor App/PortalEditor/Contentful/Client.swift
@@ -1,6 +1,12 @@
 import Contentful
 
 extension Client {
+	struct Constants {
+		static let credentialsFilename = "CMS-Credentials"
+		static let credentialsDictSpaceId = "spaceId"
+		static let credentialsDictAccessToken = "accessToken"
+	}
+	
 	private static var contentTypes: [EntryDecodable.Type] {
 		return [CFStory.self,
 				CFPortal.self,
@@ -11,6 +17,15 @@ extension Client {
 	}
 
 	public static func preview() -> Client {
-		return Client(spaceId: "____________", accessToken: "___________________________________________", host: Host.preview, contentTypeClasses: Client.contentTypes)
+		guard
+			let url = Bundle.main.url(forResource: Constants.credentialsFilename, withExtension: "plist"),
+			let dict = NSDictionary(contentsOf: url) as? [String: String],
+			let spaceId = dict[Constants.credentialsDictSpaceId],
+			let accessToken = dict[Constants.credentialsDictAccessToken]
+		else {
+			fatalError()
+		}
+
+		return Client(spaceId: spaceId, accessToken: accessToken, host: Host.preview, contentTypeClasses: Client.contentTypes)
 	}
 }
